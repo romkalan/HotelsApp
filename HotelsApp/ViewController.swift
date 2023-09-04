d969a804c01210c5ab045f166267c16806677997
romkalan <romkalan@mail.ru>
2023-09-04 18:42:33 +0300

    Initial Commit

---
+++ HotelsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotelsApp
+//
+//  Created by Roman Lantsov on 04.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
